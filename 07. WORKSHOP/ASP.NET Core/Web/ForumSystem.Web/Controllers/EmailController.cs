d522f0e60fab1ad96bbdb069a31b4e69cdddb5c4
Shtekata <gesheval@gmail.com>
2020-03-13 01:28:06 +0200

    07/Category controller

---
+++ 07. WORKSHOP/ASP.NET Core/Web/ForumSystem.Web/Controllers/EmailController.cs
@@ -0,0 +1,24 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using ForumSystem.Services.Messaging;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class EmailController : Controller
+    {
+        private readonly IEmailSender sender;
+
+        public EmailController(IEmailSender sender)
+        {
+            this.sender = sender;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index()
+        {
+            await this.sender.SendEmailAsync("gesheval@gmail.com", "Asen", "siichon.siichon@gmail.com", "Hello", "<h1>Hi</h1>");
+            return this.Redirect("/");
+        }
+    }
+}
