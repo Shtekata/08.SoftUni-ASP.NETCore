176c429b2e6205e8f0d47c5ba57b4969b5680783
Shtekata <gesheval@gmail.com>
2020-03-23 23:43:27 +0200

    12

---
+++ 12. TESTING/ForumSystem/Tests/ForumSystem.Services.Tests/VotesServiceTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ForumSystem.Services.Tests
+{
+    public class VotesServiceTest
+    {
+        [Fact]
+        public void TwoDownVotesShouldCountOnce()
+        {
+
+        }
+    }
+}
