65b71ffc9d4d998756945e4a0888d40e65037809
Shtekata <gesheval@gmail.com>
2020-04-15 17:03:04 +0300

    SGK

--- 16. WORKSHOP/ForumSystem/ForumSystem.Common/GlobalConstants.cs
+++ 16. WORKSHOP/ForumSystem/ForumSystem.Common/GlobalConstants.cs
@@ -6,6 +6,6 @@
 
         public const string AdministratorRoleName = "Administrator";
 
-        public const string SendGrid = "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";
+        public const string Send = "";
     }
 }
